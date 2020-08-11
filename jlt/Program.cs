9dc6a17c459de5b486363215f32a37429bf3adac
uniq <uniq@o2.pl>
2020-08-11 13:34:56 +0200

    CommandLineParser, plus --pretty and --asString options

--- jlt/Program.cs
+++ jlt/Program.cs
@@ -1,4 +1,5 @@
-﻿using jsonLTParser.interpreter;
+﻿using CommandLine;
+using jsonLTParser.interpreter;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,33 +9,27 @@ namespace jlt
 {
     class Program
     {
-        private const string JSONSTRING = "jsonString";
-        private const string JSONPATH = "jsonPath";
-        private const string JLTSTRING = "jltString";
-        private const string JLTPATH = "jltPath";
+        public class Options {
+            [Option('p', "pretty", Required = false, HelpText = "Pretty print.")]
+            public bool Pretty { get; set; }
 
-        static Dictionary<string, string> OrganizeArgs(string[] args, string[] available) {
-            Dictionary<string, string> options = new Dictionary<string, string>();
-            for (int i = 0; i < args.Length; i += 2) {
-                string arg = args[i];
-                if (!arg.StartsWith("-")) {
-                    Console.Error.WriteLine("all parameters should start with -. I'm looking at you: " + arg);
-                    return null;
-                }
-                arg = arg.Substring(1);
-                if (!available.Contains(arg)) {
-                    Console.Error.WriteLine("Wrong option :" + arg);
-                    Console.Error.WriteLine("Only those parameters are valid:");
-                    available.ToList().ForEach(o => Console.Error.WriteLine("  -" + o));
-                    return null;
-                }
-                if (i + 1 >= args.Length) {
-                    Console.Error.WriteLine("parameter must be followed by something. I'm looking at you: " + arg);
-                    return null;
-                }
-                options.Add(arg, args[i + 1]);
-            }
-            return options;
+            [Option('s', "asString", Required = false, HelpText = "Returns string instead of json.")]
+            public bool AsString { get; set; }
+
+            [Option("jltPath", Group = "jlt source")]
+            public string JltPath { get; set; }
+
+            [Option("jltString", Group = "jlt source")]
+            public string JltString { get; set; }
+
+            [Option("jsonPath", Group = "json source")]
+            public string JsonPath { get; set; }
+
+            [Option("jsonString", Group = "json source")]
+            public string JsonString { get; set; }
+
+            [Option('v', "verbose", Required = false, HelpText = "Verbose (prints out jltString and jsonString).")]
+            public bool Verbose { get; set; }
         }
 
         private static string ReadFile(string filePath) {
@@ -52,41 +47,31 @@ namespace jlt
         }
 
         static void Main(string[] args) {
-            Dictionary<string, string> options = OrganizeArgs(args, new string[] { JSONSTRING, JSONPATH, JLTSTRING, JLTPATH });
-            if (options == null) {
-                Usage();
-                return;
-            }
-            string json, jlt;
-            if (options.ContainsKey(JSONSTRING)) {
-                json = options[JSONSTRING];
-            } else if (options.ContainsKey(JSONPATH)) {
-                json = ReadFile(options[JSONPATH]);
-            } else {
-                Console.Error.WriteLine("I need json");
-                Usage();
-                return;
-            }
-            if (options.ContainsKey(JLTSTRING)) {
-                jlt = options[JLTSTRING];
-            } else if (options.ContainsKey(JLTPATH)) {
-                jlt = ReadFile(options[JLTPATH]);
-            } else {
-                Console.Error.WriteLine("I need jlt");
-                Usage();
-                return;
-            }
-            JsonLTInterpreter interpreter = new JsonLTInterpreter();
-            string result = interpreter.Run(json, jlt);
-            Console.WriteLine(result);
-        }
-
-        private static void Usage() {
-            Console.WriteLine("Usage:");
-            Console.WriteLine("  jlt -{0} somePath -{1} somePath", JSONPATH, JLTPATH);
-            Console.WriteLine("  jlt -{0} \"{{json}}\" -{1} \"{{jlt}}\"", JSONSTRING, JLTSTRING);
-            Console.WriteLine("  jlt -{0} \"{{json}}\" -{1} somePath", JSONSTRING, JLTPATH);
-            Console.WriteLine("  jlt -{0} somePath -{1} \"{{jlt}}\"", JSONPATH, JLTPATH);
+            String json = null;
+            String jlt = null;
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => {
+                if (o.JltPath != null) {
+                    jlt = ReadFile(o.JltPath);
+                } else if (o.JltString != null) {
+                    jlt = o.JltString;
+                    if (o.Verbose) {
+                        Console.WriteLine("jlt:");
+                        Console.WriteLine(jlt);
+                    }
+                }
+                if (o.JsonPath != null) {
+                    json = ReadFile(o.JsonPath);
+                } else if (o.JsonString != null) {
+                    json = o.JsonString;
+                    if (o.Verbose) {
+                        Console.WriteLine("json:");
+                        Console.WriteLine(json);
+                    }
+                }
+                JsonLTInterpreter interpreter = new JsonLTInterpreter();
+                string result = interpreter.Run(json, jlt, o.AsString, o.Pretty);
+                Console.WriteLine(result);
+            });
         }
     }
 }
